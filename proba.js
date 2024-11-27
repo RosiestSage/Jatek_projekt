c58a66f4ce697f8e904701ef2122908f810ac034
RosiestSage <pali.david@students.jedlik.eu>
2024-11-27 10:47:03 +0100

    #3 szinte kész, todo: hatter

---
+++ proba.js
@@ -0,0 +1,37 @@
+function tovabb(elem){
+    let lista = ["open_page","difficulty", "levels", "gameplay"];
+    let current = document.getElementById(elem);
+    switch (elem){
+        case 2:
+            document.getElementById(lista[elem - 2]).style.display = 'none';
+            document.getElementById(lista[elem -1]).style.display = 'block';
+            break;
+        case 3:
+            document.getElementById(lista[elem - 2]).style.display = 'none';
+            document.getElementById(lista[elem - 1]).style.display = 'block';
+            break;
+        case 4:
+            document.getElementById(lista[elem - 2]).style.display = 'none';
+            document.getElementById(lista[elem - 1]).style.display = 'block';
+            break;
+    }
+}
+
+function vissza(elem){
+    let lista = ["open_page","difficulty", "levels", "gameplay"];
+    let current = document.getElementById(elem);
+    switch (elem){
+        case 1:
+            document.getElementById(lista[elem - 1]).style.display = 'block';
+            document.getElementById(lista[elem]).style.display = 'none';
+            break;
+        case 2:
+            document.getElementById(lista[elem - 1]).style.display = 'block';
+            document.getElementById(lista[elem]).style.display = 'none';
+            break;        
+        case 3:
+            document.getElementById(lista[elem - 1]).style.display = 'block';
+            document.getElementById(lista[elem]).style.display = 'none';
+            break;
+    }
+}
