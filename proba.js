f4d3ee0f8fca729bb7f25395ae137d2596c484c6
RosiestSage <pali.david@students.jedlik.eu>
2024-11-27 12:47:28 +0100

    #3 kész nyitó oldal

--- proba.js
+++ proba.js
@@ -1,6 +1,5 @@
 function tovabb(elem){
     let lista = ["open_page","difficulty", "levels", "gameplay"];
-    let current = document.getElementById(elem);
     switch (elem){
         case 2:
             document.getElementById(lista[elem - 2]).style.display = 'none';
@@ -19,12 +18,7 @@ function tovabb(elem){
 
 function vissza(elem){
     let lista = ["open_page","difficulty", "levels", "gameplay"];
-    let current = document.getElementById(elem);
     switch (elem){
-        case 1:
-            document.getElementById(lista[elem - 1]).style.display = 'block';
-            document.getElementById(lista[elem]).style.display = 'none';
-            break;
         case 2:
             document.getElementById(lista[elem - 1]).style.display = 'block';
             document.getElementById(lista[elem]).style.display = 'none';
