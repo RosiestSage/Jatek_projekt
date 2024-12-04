5a4fafe4d5f0cf67dc4d63827534d67e098220b6
RosiestSage <pali.david@students.jedlik.eu>
2024-12-04 11:58:32 +0100

    menu

--- proba.js
+++ proba.js
@@ -340,14 +340,19 @@ function tovabb(elem, dif){
 
 function vissza(elem){
     let lista = ["open_page","difficulty", "levels", "gameplay"];
+    document.getElementById('menu').style.display = 'flex';
     switch (elem){
         case 2:
-            document.getElementById(lista[elem - 1]).style.display = 'block';
-            document.getElementById(lista[elem]).style.display = 'none';
+            
+
+            // document.getElementById(lista[elem - 1]).style.display = 'block';
+            // document.getElementById(lista[elem]).style.display = 'none';
             break;        
         case 3:
-            document.getElementById(lista[elem - 1]).style.display = 'block';
-            document.getElementById(lista[elem]).style.display = 'none';
+            document.getElementById("menu").innerHTML  += '<button> Back to levels</button>' + '<button> Back to difficulty</button>' + '<button> Back to game</button>';
+            // document.getElementById(lista[elem - 1]).style.display = 'block';
+            // document.getElementById(lista[elem]).style.display = 'none';
+
             break;
     }
 }
